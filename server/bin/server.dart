e8487bb73a55014bc18626f71d58bc776de2c2fa
Ayush Chothe <ayushmchothe@gmail.com>
2023-06-18 20:47:04 +0530

    ip fix

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -9,7 +9,7 @@ import 'src/handler.dart';
 void main(List<String> args) async {
   try {
     // Use any available host or container IP (usually `0.0.0.0`).
-    final ip = InternetAddress.loopbackIPv4;
+    final ip = InternetAddress.tryParse('0.0.0.0')!;
     // For running in containers, we respect the PORT environment variable.
     final port = int.parse(Platform.environment['PORT'] ?? '3000');
     await supabase.clearPayloads();
